cc2b6f3d430339609af496da1dbf894197a978a0
Ben McGunigle <bnmcgn@gmail.com>
2015-10-13 06:10:57 -0700

    Adjust dependency imports

--- html-package.lisp
+++ html-package.lisp
@@ -1,7 +1,7 @@
 (defpackage #:html-thing-lister
   (:use #:cl #:cl-who #:thing-lister #:gadgets
 	#:alexandria #:anaphora #:webhax #:thing-labels #:parenscript)
-  (:shadowing-import-from #:webhax #:str)
+  (:shadowing-import-from #:webhax #:str #:call)
   (:shadowing-import-from #:parenscript #:switch)
   (:export #:*thing-display-set*
 	   #:thing-display-core
